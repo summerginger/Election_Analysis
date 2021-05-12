ccc6111421f25cfa17a174e0afa2624d7fd012a2
summerginger <janehcanada@gmail.com>
2021-05-12 10:40:41 -0400

    adding the election analysis

---
+++ pypoll.py
@@ -0,0 +1,44 @@
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# Add our dependencies.
+import csv
+import os
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+     txt_file.write("counties in the Election\n---------------\nArapahoe\nDenver\nJefferson")
+     
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+ # To do: read and analyze the data here.
+# Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+# Print each row in the CSV file.
+    headers = next(file_reader)
+    print(headers)
